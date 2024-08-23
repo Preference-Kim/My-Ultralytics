a0325816cf343b6680196ea5381749432a9d367f
Preference-Kim <preferencekimm@gmail.com>
2024-08-23 14:45:01 +0900

    initial commits for myuiltralytics package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="myultralytics",
+    version="0.1.0",
+    description="A custom YOLO implementation for object detection",
+    packages=find_packages(where='./myultralytics'),
+    package_dir={'': 'myultralytics'},
+    install_requires=[
+        "ultralytics",
+        "onnxruntime",
+    ],
+    python_requires=">=3.12.4",
+)
--- topics/custom_dataset_integration/train.py
+++ topics/custom_dataset_integration/train.py
@@ -1,17 +1,44 @@
 import yaml
 from pathlib import Path
-
-from .trainer import MyDetectionTrainer
+from myultralytics.trainer import MyDetectionTrainer
 
 """
+Prerequisites:
+    install `myultralytics` package on root
+    ```bash
+        pip install -e .
+    ```
+
 Example:
     ```bash
-    python -m topics.custom_dataset_integration.train
+        export PIN_MEMORY=False # Optional
+        python topics/custom_dataset_integration/train.py
     ```
 """
 
 
 CFG_PATH = Path(__file__).parent / 'cfg'
+
+
+def freeze_layer(trainer):
+    """
+    NOTE:
+
+    This callback doesn't support at DDP.
+    If a fine training with multi-GPUs is needed, add `freeze` option in `train.yaml`
+    """
+    
+    num_freeze = 10 # backbone
+    
+    model = trainer.model
+    print(f"Freezing {num_freeze} layers")
+    freeze = [f'model.{x}.' for x in range(num_freeze)]  # layers to freeze 
+    for k, v in model.named_parameters(): 
+        v.requires_grad = True  # train all layers 
+        if any(x in k for x in freeze): 
+            print(f'freezing {k}') 
+            v.requires_grad = False 
+    print(f"{num_freeze} layers are freezed.")
 
 
 if __name__ == '__main__':
@@ -23,4 +50,5 @@ if __name__ == '__main__':
     cfgs['data'] = str(data_cfg.resolve())
     
     trainer = MyDetectionTrainer(overrides=cfgs)
+    # trainer.add_callback("on_train_start", freeze_layer)
     trainer.train()
